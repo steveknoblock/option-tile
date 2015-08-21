955bbb7a59e86ca7ddc2b056d510d423b48a0215
Steve Knoblock <steve.knoblock@gmail.com>
2015-08-21 11:23:16 -0400

    The basic auto transform of select inputs to option tiles is working. I am experimenting with displaying the option value on the tile.

---
+++ optionTile.js
@@ -0,0 +1,40 @@
+
+// optionTile
+// Transforms marked select inputs into option tile menus
+// using them as data sources.
+// Steve Knoblock 2015
+// MIT License
+
+var optionTiles = '';
+
+// iterate over source selects
+$('select.option-tile-source').each(function(ind, val){
+
+	// this contains the selected element and all of its children
+	optionTiles += '<div class="option-surface">';
+
+	optionTiles += '    <ul id="' + $( this ).attr( 'id' ) + '" class="option-tiles">';
+
+	$.each(this, function(index, value) {
+	
+		optionTiles += '        <li id="hello" data-select-index="' + $( this ).attr( 'value' ) + '"><span>' + $( this ).text() + '</span></li>';
+
+	})
+
+	optionTiles += '</ul></div>';
+
+});
+
+$("body").append(optionTiles);
+
+$('.option-tiles li').each(function(index) {
+	console.log("Adding click handler.");
+	$(this).on('click', function(event) {
+		event.stopPropagation();
+		var data = $(this).attr('data-select-index');
+		var id = $(this).parent().attr('id');
+		//console.log("Id: " + id);
+		//console.log("Data: " + data);
+		$('#' + id).val(data);
+	});
+});
